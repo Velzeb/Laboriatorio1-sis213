04e288ab55dd121d916a1de8e08f1ad954f68c9a
velzebV <carlos.pacoricona@ucb.edu.bo>
2024-02-15 16:43:41 -0400

    editado

--- Pantalla.java
+++ Pantalla.java
@@ -8,7 +8,11 @@ public class Pantalla {
 		System.out.println("APLICACION");
 		System.out.print("INTEGRANTES: ");
 		System.out.print("Carlos Pacoricona: ");
-		System.out.print("Ruddy Beltran");
+		System.out.print(" ");
+		System.out.print(" Encabezado");
+		System.out.print(" Crear una aplicacion de lista de tareas que permita a los usuarios agregar nuevas tareas, marcarlas como completadas, eliminarlas de la lista, generar reportes por estados de tareas en curso y tareas completas");
+
+		
 		
 	}
 
