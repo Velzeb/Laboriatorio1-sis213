8f488d488cd6b1573eb87510b0a32598f0cb8cd5
velzebV <carlos.pacoricona@ucb.edu.bo>
2024-02-15 16:23:44 -0400

    agregado de pantalla principal

---
+++ Pantalla.java
@@ -0,0 +1,15 @@
+
+public class Pantalla {
+
+	public static void main(String[] args) {
+
+		System.out.println("******");
+		System.out.println("SIS-213");
+		System.out.println("APLICACION");
+		System.out.print("INTEGRANTES: ");
+		System.out.print("Carlos Pacoricona: ");
+		System.out.print("Ruddy ");
+		
+	}
+
+}
